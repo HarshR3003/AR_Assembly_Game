0da1b2a6c296c54bba91727b8b585a9542952b7b
Harsh <harsh_r@me.iitr.ac.in>
2024-05-16 17:25:06 +0530

    Established connection between dissassembly and assembly level. Inisialised assembly script.

--- Assets/Scripts/Controller2.cs
+++ Assets/Scripts/Controller2.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Controller2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Script is being activated.
     void Start()
     {
         Debug.Log("Chal rha hai!");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
